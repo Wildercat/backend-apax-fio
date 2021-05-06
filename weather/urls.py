12b7c2993a465ef299d286090c0cf13ad08913cc
Abram Gornik <39809284+Wildercat@users.noreply.github.com>
2021-05-06 12:29:09 -0400

    user login, creation

--- weather/urls.py
+++ weather/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'weather'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:user_id>/', views.userpage, name='userpage'),
+    path('login/', views.login, name='login'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 
+from .models import User
 
 def index(request):
-    return HttpResponse("hi")
+    return render(request, 'weather/index.html')
+
+def login(request): #if user exists, goto user page, else create user then goto user page
+    username = request.POST['username']
+    try:
+        user = User.objects.get(username=username)
+    except:
+        user = User.objects.create(username=username)
+    return HttpResponseRedirect(reverse('weather:userpage', args=[user.id]))
+
+def userpage(request, user_id):
+    context = {
+        'user': get_object_or_404(User, pk=user_id)
+    }
+    return render(request, 'weather/userpage.html/', context)
